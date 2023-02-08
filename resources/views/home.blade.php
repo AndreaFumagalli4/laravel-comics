5835e2ddea4e88bbeb9309e36e11e95276543987
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2023-02-08 15:25:04 +0100

    add layout files

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('main-content')
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                <h2>
+                    Homepage
+                </h2>
+            </div>
+        </div>
+    </div>
+@endsection
