d7133ade257e815dfd21656cbaebe9b024606bd4
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2023-02-08 16:02:30 +0100

    add card structure

---
+++ resources/views/comics.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('main-content')
+    <div class="container">
+        <div class="row">
+            <div class="col-3">
+                <div class="card">
+                    <img src="" alt="">
+                    <div class="description">
+                        <h3>
+                            Title
+                        </h3>
+                        <p>
+                            Series
+                        </p>
+                        <p>
+                            Price
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
